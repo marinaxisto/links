72881e0c1bd0c56ae76d220da64fd4e80d088c6e
xistografia <153650318+xistografia@users.noreply.github.com>
2024-03-26 19:10:24 -0300

    image trigger update

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // tag img
-  const img = document.querySelector("#profile img")
+  // // tag img
+  // const img = document.querySelector("#profile img")
 
-  // replace the image
-  if (html.classList.contains("light")) {
-    // if in light mode, add light image
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    // If not in light mode, keep the default image
-    img.setAttribute("src", "./assets/avatar.png")
-  }
+  // // replace the image
+  // if (html.classList.contains("light")) {
+  //   // if in light mode, add light image
+  //   img.setAttribute("src", "./assets/avatar-light.png")
+  // } else {
+  //   // If not in light mode, keep the default image
+  //   img.setAttribute("src", "./assets/avatar.png")
+  // }
 }
